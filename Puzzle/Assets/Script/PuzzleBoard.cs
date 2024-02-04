2c7c01727f587d90d8bd3371dd4038e02da82d5b
足利 慶次 <a.keiji14@gmail.com>
2024-02-04 14:32:23 +0900

    盤面クラスのPuzzleFrameをPuzzleBoardにリネーム対応

--- Puzzle/Assets/Script/PuzzleBoard.cs
+++ Puzzle/Assets/Script/PuzzleBoard.cs
@@ -2,7 +2,7 @@
 
 namespace Puzzle
 {
-    public class PuzzleFrame : MonoBehaviour
+    public class PuzzleBoard : MonoBehaviour
     {
         #region PublicField
         /// <summary>盤面に配置済みかどうか</summary>
